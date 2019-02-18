82728baf3689a90c4382dbb6fffb770e4315a665
Fernando Herrero Carrón <fernando@parity.io>
2019-02-18 11:48:00 +0100

    Copyrights

--- ethcore/light/src/types/request/contract_code.rs
+++ ethcore/light/src/types/request/contract_code.rs
@@ -1,4 +1,21 @@
-//! contract_code
+// Copyright 2015-2019 Parity Technologies (UK) Ltd.
+// This file is part of Parity Ethereum.
+
+// Parity Ethereum is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Parity Ethereum is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Parity Ethereum.  If not, see <http://www.gnu.org/licenses/>.
+
+//! Contract code requests
+
 use super::{Field, NoSuchOutput, OutputKind, Output};
 use ethereum_types::H256;
 use bytes::Bytes;
