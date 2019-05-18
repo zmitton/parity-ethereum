0f974a18be0a14b468b92092c3c7d5cafd02748a
Michael Seiler <michaelseiler@Michaels-MacBook-Pro.local>
2019-05-18 11:36:16 -0400

    use cors

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 cargo build -j8
-./target/debug/parity   --chain mtihani.json --author 0x2958DB51a0b4c458d0aa183E8cFB4f2E95cf6E75
+./target/debug/parity   --chain mtihani.json --author 0x2958DB51a0b4c458d0aa183E8cFB4f2E95cf6E75 --jsonrpc-hosts all --jsonrpc-interface all --jsonrpc-cors '*'
+
+
